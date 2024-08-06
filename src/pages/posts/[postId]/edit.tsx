b8b4dbf70f4d2665d7946faa695cc06d4228c10f
Ashish singh <ashishsingh1331@gmail.com>
2024-08-06 18:06:52 +0530

    Users listing page

---
+++ src/pages/posts/[postId]/edit.tsx
@@ -0,0 +1,28 @@
+import { PostForm } from "@/components/PostForm";
+import { getPost } from "@/db/posts";
+import { getUsers } from "@/db/users";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+export default function EditPostPage({
+  users,
+  post,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const post: any = {};
+
+  return (
+    <>
+      <h1 className="page-title">Edit Post</h1>
+      <PostForm post={post} />
+    </>
+  );
+}
+
+export const getServerSideProps = (async ({ params }) => {
+  const [users, post] = await Promise.all([
+    getUsers(),
+    getPost(params?.postId as string),
+  ]);
+
+  if (post == null) return { notFound: true };
+  return { props: { users, post } };
+}) satisfies GetServerSideProps;
